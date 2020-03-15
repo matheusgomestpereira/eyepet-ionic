75096ec7a926724b518d754a1b2c01e0607151e0
Matheus Gomes <matheusgomestpereira@outlook.com>
2020-03-15 14:18:37 -0300

    init

--- eyepet/src/app/app.module.ts
+++ eyepet/src/app/app.module.ts
@@ -8,12 +8,14 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {APP_BASE_HREF} from '@angular/common';
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
   providers: [
+    {provide: APP_BASE_HREF, useValue: './'},
     StatusBar,
     SplashScreen,
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
