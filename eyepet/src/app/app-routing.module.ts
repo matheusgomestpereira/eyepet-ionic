f033ef790ace7336a0238d9c8bf74573799bfaa1
Matheus Gomes <matheusgomestpereira@outlook.com>
2020-04-08 09:24:11 -0300

    visor layout

--- eyepet/src/app/app-routing.module.ts
+++ eyepet/src/app/app-routing.module.ts
@@ -5,6 +5,10 @@ const routes: Routes = [
   {
     path: '',
     loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
+  },
+  {
+    path: 'historico',
+    loadChildren: () => import('./historico/historico.module').then( m => m.HistoricoPageModule)
   }
 ];
 @NgModule({
